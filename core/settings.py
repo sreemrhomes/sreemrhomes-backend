0704241c301e7592f24a7937c2a5f40430833c89
sathya7032 <sathichary581@gmail.com>
2026-05-29 12:02:01 +0530

    Hosts change

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f1xlbs5e@&pr3u1$&!()2ng6z+&)yyqk82gs87xmw*k$=vlk+a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sreemrhomes.pythonanywhere.com","127.0.0.1"]
+ALLOWED_HOSTS = ["sreemrhomess.pythonanywhere.com","127.0.0.1"]
 
 
 # Application definition
